7cbaf77890f275b9fdd1d305bb5b3845ac70a438
Isaac Ordoñez Fornós <isaac.fornos02@gmail.com>
2024-06-16 10:43:13 +0200

    Update fix.sh

--- fix.sh
+++ fix.sh
@@ -2,6 +2,7 @@
 # by isaachhk02 (Isaac Ordoñez Fornós)
 
 # Kill Xorg and Wayland process before update libdrm package
+systemd
 systemctl stop sddm
 echo "Updating libdrm (possible fix)"
 sudo dnf install libdrm -y
