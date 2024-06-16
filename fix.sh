92d06e4c98915d324afe85cafba266c9910f69a6
Isaac Ordoñez Fornós <isaac.fornos02@gmail.com>
2024-06-16 11:53:50 +0200

    Delete fix.sh

--- fix.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# by isaachhk02 (Isaac Ordoñez Fornós)
-
-# Kill Xorg and Wayland process before update libdrm package
-sudo dnf install sddm-x11 --allowerasing -y
-echo "Updating vmware tools (possible fix)"
-sudo dnf install open-vm-tools-test -y
-echo "Done!"
-startplasma-x11
